8ef6f07df0def91ed4830ed032060736b96ed8aa
Alex221100 <71218218+Alex221100@users.noreply.github.com>
2022-09-16 11:56:36 +0200

    feat(merge): merge during move left.

--- lib/main.dart
+++ lib/main.dart
@@ -39,17 +39,14 @@ class PositionedTilesState extends State<PositionedTiles> {
                 width: 450,
                 child: GestureDetector(
                     onHorizontalDragEnd: (dragEndDetails) {
-                      if (dragEndDetails.primaryVelocity! < 0) {
+                      if (dragEndDetails.primaryVelocity! < -100) {
                         // Page forwards
                         print('Move left');
+                        moveLeft();
                         //_goForward();
-                      } else if (dragEndDetails.primaryVelocity! > 0) {
+                      } else if (dragEndDetails.primaryVelocity! > 100) {
                         // Page backwards
                         print('Move right');
-                        setState(() {
-                          tiles[0][0] = tiles[0][0] + 1;
-                        });
-                        print(tiles[0][0]);
                       }
                     },
                     onVerticalDragEnd: (dragEndDetails) {
@@ -76,5 +73,22 @@ class PositionedTilesState extends State<PositionedTiles> {
     setState(() {
       tiles.insert(1, tiles.removeAt(0));
     });
+  }
+
+  moveLeft(){
+    for (int i = 0; i < tiles.length; i++){
+      for (int j = tiles.length-1; j > 0; j--){
+        if (tiles[i][j] == tiles[i][j-1] ||  tiles[i][j-1] == 0){
+          //merge
+          merge(i, j-1, i, j);
+        }
+      }
+    }
+  }
+
+  //x1 est la case qui récupére la somme
+  merge(int x1, int y1, int x2, int y2 ){
+    tiles[x1][y1] = tiles[x1][y1] + tiles[x2][y2];
+    tiles[x2][y2] = 0;
   }
 }
