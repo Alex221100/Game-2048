6773a0adf99fd0b144346970dfb04f9ccb81a55b
Dopey990 <ddope21@gmail.com>
2022-09-26 16:26:39 +0200

    Added score and scoring mechanics

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+import 'score.dart';
 import 'tile.dart';
 
 void main() {
@@ -34,73 +35,104 @@ class PositionedTilesState extends State<PositionedTiles> {
     String? swipeDirection;
 
     return Scaffold(
-        body: Center(
-            child: Container(
-                color: const Color.fromRGBO(187, 173, 160, 1),
-                height: 450,
-                width: 450,
-                child: GestureDetector(
-                    onPanUpdate: (details) {
-                      if (details.delta.dx > 1) {
-                        swipeDirection = "right";
+        body: InheritedScore(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+          Center(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                SizedBox(
+                    height: 50,
+                    width: 225,
+                    child: Container(
+                        color: Colors.orangeAccent,
+                        child: Center(child: Builder(builder: (context) {
+                          return Text(
+                              "Best Score : ${InheritedScore.of(context)?.scoreStructure.bestScore}",
+                              style: const TextStyle(fontSize: 18.0));
+                        })))),
+                SizedBox(
+                    height: 50,
+                    width: 225,
+                    child: Container(
+                        color: Colors.amberAccent,
+                        child: Center(child: Builder(builder: (context) {
+                          return Text(
+                              "Current Score : ${InheritedScore.of(context)?.scoreStructure.currentScore}",
+                              style: const TextStyle(fontSize: 18.0));
+                        }))))
+              ])),
+          Container(
+              color: const Color.fromRGBO(187, 173, 160, 1),
+              height: 450,
+              width: 450,
+              child: GestureDetector(
+                  onPanUpdate: (details) {
+                    if (details.delta.dx > 1) {
+                      swipeDirection = "right";
+                    } else {
+                      if (details.delta.dx < -1) {
+                        swipeDirection = "left";
                       } else {
-                        if (details.delta.dx < -1) {
-                          swipeDirection = "left";
+                        if (details.delta.dy > 1) {
+                          swipeDirection = "down";
                         } else {
-                          if (details.delta.dy > 1) {
-                            swipeDirection = "down";
-                          } else {
-                            if (details.delta.dy < -1) {
-                              swipeDirection = "up";
-                            }
+                          if (details.delta.dy < -1) {
+                            swipeDirection = "up";
                           }
                         }
                       }
-                    },
-                    onPanEnd: (details) {
-                      if (swipeDirection != null) {
-                        if (swipeDirection == "right") {
-                          print("Action right");
+                    }
+                  },
+                  onPanEnd: (details) {
+                    if (swipeDirection != null) {
+                      if (swipeDirection == "right") {
+                        print("Action right");
+                        setState(() {
+                          move(0, tiles.length - 1, tiles.length, 0, 1, -1);
+                        });
+                      } else {
+                        if (swipeDirection == "left") {
+                          print("Action left");
                           setState(() {
-                            move(0,tiles.length-1, tiles.length, 0, 1,-1);
+                            move(0, 0, tiles.length, tiles.length - 1, 1, 1);
                           });
                         } else {
-                          if (swipeDirection == "left") {
-                            print("Action left");
-                            setState(() {
-                              move(0,0,tiles.length, tiles.length-1,1,1);
-                            });
+                          if (swipeDirection == "down") {
+                            print("Action down");
                           } else {
-                            if (swipeDirection == "down") {
-                              print("Action down");
-
-                            } else {
-                              if (swipeDirection == "up") {
-                                print("Action up");
-                              }
+                            if (swipeDirection == "up") {
+                              print("Action up");
                             }
                           }
                         }
                       }
-                    },
-                    child: GridView.count(
-                        crossAxisCount: 4,
-                        children: tiles
-                            .expand((value) => value)
-                            .map<StatefulColorfulTile>((element) =>
-                                StatefulColorfulTile(element, key: UniqueKey()))
-                            .toList())))));
+                    }
+                  },
+                  child: GridView.count(
+                      crossAxisCount: 4,
+                      children: tiles
+                          .expand((value) => value)
+                          .map<StatefulColorfulTile>((element) =>
+                              StatefulColorfulTile(element, key: UniqueKey()))
+                          .toList())))
+        ])));
   }
 
   move(int istart, int jstart, int iend, int jend, int ifactor, int jfactor) {
-    for (int i = istart; i < iend; i+=ifactor) {
-      for (int j = jstart ; jfactor * j < jend * jfactor; j+=jfactor) {
-        if (tiles[i][j] == tiles[i][j + 1 * jfactor] || tiles[i][j + 1*jfactor] == 0) {
+    for (int i = istart; i < iend; i += ifactor) {
+      for (int j = jstart; jfactor * j < jend * jfactor; j += jfactor) {
+        if (tiles[i][j] == tiles[i][j + 1 * jfactor] ||
+            tiles[i][j + 1 * jfactor] == 0) {
           //merge
-          if (j-1*jfactor > 0 && j-1*jfactor < tiles.length && tiles[i][j-1*jfactor] == 0){
-            merge(i, j , i, j+1*jfactor, i, j-1*jfactor);
-          }else{
-            merge(i, j , i, j+1*jfactor, i, j);
+          if (j - 1 * jfactor > 0 &&
+              j - 1 * jfactor < tiles.length &&
+              tiles[i][j - 1 * jfactor] == 0) {
+            merge(i, j, i, j + 1 * jfactor, i, j - 1 * jfactor);
+          } else {
+            merge(i, j, i, j + 1 * jfactor, i, j);
           }
         }
       }
@@ -111,8 +143,31 @@ class PositionedTilesState extends State<PositionedTiles> {
   merge(int x1, int y1, int x2, int y2, int mergex, int mergey) {
     tiles[mergex][mergey] = tiles[x1][y1] + tiles[x2][y2];
     tiles[x2][y2] = 0;
-    if (mergey != y1){
+    if (mergey != y1) {
       tiles[x1][y1] = 0;
     }
   }
+
+  spawnRandomTile() {
+    List<Pair<int, int>> availablePlaces = [];
+
+    for (int i = 0; i < tiles.length; i++) {
+      for (int j = tiles.length - 1; j > 0; j--) {
+        if (tiles[i][j] == 0) {
+          availablePlaces.add(Pair(i, j));
+        }
+      }
+    }
+
+    Pair<int, int> randomPick =
+        availablePlaces[Random().nextInt(availablePlaces.length)];
+    tiles[randomPick.x][randomPick.y] = 2;
+  }
+}
+
+class Pair<T1, T2> {
+  final T1 x;
+  final T2 y;
+
+  Pair(this.x, this.y);
 }
---
+++ lib/score.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ScoreStructure {
+  int currentScore = 0;
+  int bestScore = 0;
+
+  setCurrentScore(int newValue) {
+    currentScore + newValue;
+  }
+
+  setBestScore(int newValue) {
+    if (newValue > bestScore) {
+      bestScore = newValue;
+    }
+  }
+}
+
+class InheritedScore extends InheritedWidget {
+  final ScoreStructure scoreStructure = ScoreStructure();
+
+  InheritedScore({Key? key, required child})
+      : assert(child != null),
+        super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(InheritedScore oldWidget) =>
+      oldWidget.scoreStructure.currentScore != scoreStructure.currentScore ||
+      oldWidget.scoreStructure.bestScore != scoreStructure.bestScore;
+
+  static InheritedScore? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<InheritedScore>();
+  }
+
+  setCurrentScore(int newValue) {
+    scoreStructure.setCurrentScore(newValue);
+  }
+
+  setBestScore(int newValue) {
+    scoreStructure.setBestScore(newValue);
+  }
+}
